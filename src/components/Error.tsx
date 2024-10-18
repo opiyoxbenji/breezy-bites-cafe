5c6e463dff5a246879cc155b5f0d7729dd208593
pixelbushi <opiyosbenji@gmail.com>
2024-10-18 11:03:00 +0300

    created a components folder for different components of the website

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Error = (props: Props) => {
+  return (
+    <div>404</div>
+  )
+}
+
+export default Error
---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Error from "./Error";
+import Hero from "./Hero";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  Error,
+}
